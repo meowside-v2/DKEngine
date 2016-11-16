9ed1815b5d4ab3a0945bfe24e4f933b71d2d47d5
xmuf <xmuf@DESKTOP-AJVTE6Q>
2016-11-16 17:21:44 +0100

    16/11/2016

--- DKEngine/DKEngine/Program.cs
+++ DKEngine/DKEngine/Program.cs
@@ -14,61 +14,7 @@ namespace DKEngine
     {
         static void Main(string[] args)
         {
-            Database.InitDatabase();
 
-            Database.AddNewGameObject("gif", new Material(Image.FromFile(@"asd.gif")));
-
-            Camera cam = new Camera();
-
-            cam.sceneReference = new Scene();
-
-            GameObject g1 = new GameObject()
-            {
-                ScaleX = 2,
-                ScaleY = 2,
-                ScaleZ = 2,
-                TypeName = "gif"
-            };
-
-            GameObject g2 = new GameObject()
-            {
-                X = g1.width,
-                ScaleX = 2,
-                ScaleY = 2,
-                ScaleZ = 2,
-                TypeName = "gif"
-            };
-
-            GameObject g3 = new GameObject()
-            {
-                X = g2.width,
-                ScaleX = 2,
-                ScaleY = 2,
-                ScaleZ = 2,
-                TypeName = "gif"
-            };
-
-            Play(g1, g2, g3);
-
-            cam.sceneReference.Model.AddAll(g1, g2, g3);
-
-            cam.Init(0, 0);
-
-            Console.ReadLine();
-        }
-
-        static void Play(params GameObject[] g)
-        {
-            foreach (GameObject x in g)
-            {
-                Timer t = new Timer((s) =>
-                {
-                    x.AnimationState++;
-
-                    if (x.AnimationState >= x.model.Frames)
-                        x.AnimationState = 0;
-                }, null, 0, x.model.DurationPerFrame);
-            }
         }
     }
 }
