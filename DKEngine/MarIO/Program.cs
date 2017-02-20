80f70f7868a149146fd70e9bfa29d8ba8841809b
xmuf <xmuf@DESKTOP-AJVTE6Q>
2017-02-20 08:49:46 +0100

    First MarIO commit

---
+++ DKEngine/MarIO/Program.cs
@@ -0,0 +1,21 @@
+﻿using DKBasicEngine_1_0;
+using DKBasicEngine_1_0.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine.Init();
+            Database.LoadResources(Sprites.Sprites.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true));
+
+        }
+    }
+}
